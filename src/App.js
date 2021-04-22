9ba102d1740566ff60f8ccc2109658bd8606633b
Wstld <none@none.com>
2021-04-22 14:24:17 +0200

    dogprofile page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import WelcomePage from './pages/welcome_pg';
 import RegisterPage from './pages/register_pg';
@@ -9,16 +9,36 @@ import Header from './components/Header';
 import DogProfile from './pages/dogProfile_pg';
 
 function App() {
+  let dog = {
+    name: "Molly",
+    sex: "female",
+    breed: "briard",
+    img: "https://images.dog.ceo/breeds/briard/n02105251_6840.jpg",
+    present: false,
+    age: 4,
+    chipNumber: "IEH455006",
+    owner: {
+      name: "Wilmer",
+      lastName: "Svensson",
+      phoneNumber: "0769239356"
+    }
+  }
   return (
-    <div className="App">
-      <Header/>
-      <DogProfile/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/"> <WelcomePage/> </Route>
+          <Route path="/register">  <RegisterPage/> </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
 export default App;
-/* 
+/*
 api hundar https://api.jsonbin.io/b/5f4d604b514ec5112d136cd6
 
 Trevlig välkomstsida där man hälsas välkommen
@@ -26,7 +46,7 @@ Register där samtliga kunder som registrerats finns med ( dogs.json )
 Info om varje hund som finns i registret layoutat och CSS:at vackert
 
 
-För VG krävs dessutom 
+För VG krävs dessutom
 att WebbAppen är anpassade både för mobil och desktop.
 
 Och att antingen:
--- src/pages/dogProfile_pg.js
+++ src/pages/dogProfile_pg.js
@@ -1,29 +1,67 @@
 import React from 'react'
 import './dogProfile_pg.css'
 
+const GenderBox  = (props) => {
+   if(props.gender.toLowerCase() == "male"){
+       return  <div className="dog-profile__small-cont">
+       <i className="fas fa-mars dog-profile__gender"/>
+        </div>
+   }else if (props.gender.toLowerCase() == 'female'){
+       return  <div className="dog-profile__small-cont">
+       <i className="fas fa-venus dog-profile__gender"/>
+   </div>
+   }else{
+       return  <div className="dog-profile__small-cont">
+       <i className="fas fa-venus-mars dog-profile__gender"/>
+   </div>
+   }
+}
+
 const DogProfile = (props) => {
     return(
         <>
         <div className="dog-profile__main-cont">
+            <div className="dog-profile__img" style={{ backgroundImage:`${props.dog.img}` }}/>
+
             <div className="dog-profile__dog-cont">
-                <div className="dog-profile__firstrow">
-                    <h3>7 y.o</h3>
-                    <div className="dog-profile__img"/>
-                    <i className="fas fa-mars dog-profile__gender"/>
+                <div className="dog-profile__name-outer-cont">
+                    <div className="dog-profile__name-inner-cont">
+                        <h2 className="dog-profile__name">{props.dog.name}</h2>
+                        <p>{props.dog.breed}</p>
+                        <div className="dog-profile__age-gen-box">
+                            <div className="dog-profile__small-cont">
+                                <h2 className="dog-profile__age">{props.dog.age}</h2>
+                                <p className="dog-profile__age-sub">y.o</p>
+                            </div>
+                            <GenderBox gender= {props.dog.sex}/>
+                            
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className="dog-profile__secondrow">
-                    <h2 className="dog-profile__name">Molly</h2>
+
+                
+
+                <div className="dog-profile__owner-cont">
+                    <h1 className="dog-profile__owner-txt">OWNER</h1>
+                    <h2 className="dog-profile__owner-txt">{`${props.dog.owner.name} ${props.dog.owner.lastName} `}</h2>
+                    <h3 className="dog-profile__owner-txt"><i className="fas fa-phone"></i> {props.dog.owner.phoneNumber}</h3>
+                    <div className="dog-profile__nrbox">
+                        <p>chipnr:</p>
+                        <h2>{props.dog.chipNumber}</h2>
+                    </div>
                 </div>
-                <div className="dog-profile__thirdrow">
-                    <h2>Breed: something</h2>
-                    <h2>Chipnr: dasdvasdvv</h2>
-                </div>
+
+
+               
+               
+
+             
+
+
             </div>
-            <div className="dog-profile__owner-cont">
-                <h1>OWNER</h1>
-                <h2>Erik johansson</h2>
-                <h3><i className="fas fa-phone"></i> 0706567890</h3>
-            </div>
+        
+           
 
         </div>
         </>
@@ -31,3 +69,35 @@ const DogProfile = (props) => {
 }
 
 export default DogProfile
+
+/*<i className="fas fa-mars dog-profile__gender"/>
+    <div className="dog-profile__dog-cont">
+               
+                <div className="dog-profile__firstrow">
+                 
+
+                   
+                </div>
+
+               
+
+                <div className="dog-profile__secondrow">
+                    <div className="dog-profile__align-row">
+                        <h2 className="dog-profile__txt">BREED: something</h2>
+                        <h2 className="dog-profile__txt">CHIP: dasdvasdvv</h2>
+                    </div>
+                    
+                </div>
+                
+            
+              
+            </div>
+
+            
+
+            <div className="dog-profile__owner-cont">
+                <h1 className="dog-profile__owner-txt">OWNER</h1>
+                <h2 className="dog-profile__owner-txt">Erik johansson</h2>
+                <h3 className="dog-profile__owner-txt"><i className="fas fa-phone"></i> 0706567890</h3>
+            </div>
+*/ 
